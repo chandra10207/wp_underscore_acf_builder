40957f42b6a979d3e1f6c6ab4c8b07d5b46a49fe
Chandra Pandey <chandra10207@gmail.com>
2025-06-16 17:15:58 +1000

    vendor files added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'understrap/understrap-child',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8ded97c95bb4c51e0feb0e508700d8ba9127b6c9',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'doctrine/inflector' => array(
+            'pretty_version' => '2.0.10',
+            'version' => '2.0.10.0',
+            'reference' => '5817d0659c5b50c9b950feb9af7b9668e2c436bc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../doctrine/inflector',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stoutlogic/acf-builder' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'reference' => 'e63ab87233ea3675cd519f1dd6b6d4230b3cef97',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stoutlogic/acf-builder',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'understrap/understrap-child' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8ded97c95bb4c51e0feb0e508700d8ba9127b6c9',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
